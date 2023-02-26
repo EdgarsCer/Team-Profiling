c5a611999ba0ba94b8a6217398ee973de23216ac
EdgarsCer <Edgarscernijs1993@gmail.com>
2023-02-26 14:21:54 +0000

    Sll tests are passing! some of index.js started

---
+++ starter/index.js
@@ -0,0 +1,76 @@
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const inquirer = require("inquirer");
+const path = require("path");
+const fs = require("fs");
+const render = require("./src/page-template.js");
+
+const OUTPUT_DIR = path.resolve(__dirname, "output");
+const outputPath = path.join(OUTPUT_DIR, "team.html");
+
+
+
+// TODO: Write Code to gather information about the development team members, and render the HTML file.
+
+ let team = [];
+
+ async function startProgram(){
+    const managerData = await inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: "What is the manager's name?",
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: "What is the manager's ID?",
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: "What is the manager's email?",
+        },
+        {
+            type: 'input',
+            name: 'officeNumber',
+            message: "What is the manager's office number?",
+        },
+    ]);
+
+    const manager = new Manager(managerData.name, managerData.id, managerData.email, managerData.officeNumber);
+    team.push(manager);
+
+    
+    let htmlDoc = render(team);
+    await fs.writeFile(outputPath, htmlDoc, () => {});
+}
+
+
+
+
+
+
+const engineerData = await inquirer.prompt([
+    {
+        type: 'input',
+        name: 'name',
+        message: "What is the engineer's name?",
+    },
+    {
+        type: 'input',
+        name: 'id',
+        message: "What is the engineer's ID?",
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: "What is the engineer's email?",
+    },
+    {
+        type: 'input',
+        name: 'officeNumber',
+        message: "What is the engineer's GitHub User Name?",
+    },
+]);
